66ebab48ca3e71c26cd2ad4db0b06faac54f2a10
kapil <kapsstudy@gmail.com>
2021-06-10 23:19:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var root = document.getElementById("root");
+var url = "https://robohash.org/";
+
+function randomGen() {
+    return (
+        Math.floor(Math.random() * 1000)
+    );
+}
+
+function infinity() {
+    var i = 0;
+    while (i < 20) {
+        const img = document.createElement("img");
+        img.src = `${url}${randomGen()}`;
+        root.appendChild(img);
+        i++
+    }
+}
+infinity();
+window.addEventListener("scroll", () => {
+    if ((window.scrollY + window.innerHeight + window.innerWidth) >= document.documentElement.scrollHeight) {
+        infinity();
+    }
+})
